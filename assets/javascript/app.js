b685b2b881a9e8d803529e1c7500677a6736b12c
Ryan Holt <ryanholt22@yahoo.com>
2019-07-30 23:15:04 -0500

    added media queries, changed a bit of the JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,9 +6,52 @@ we have p tags with ids for each answer in the html. we will add buttons to the 
 
 var time = $("#timeRem");
 
+$("#startTimer").on("click", function () {
+    var counter = 30;
+    setInterval(function() {
+        counter--;
+        if (counter >= 0) {
+            span = document.getElementById("timeRem");
+            span.innerHTML = counter;
+        }
+        if (counter === -1) {
+            alert("Times Up! lets see what you have correct");
+            clearInterval(counter);
+        }
+    }, 1000);
+});
 
 
+$("#one").on("click", function() {
+    alert("Correct! Zoom!");
+});
 
+$("#two").on("click", function () {
+    alert("Incorrect!");
+});
 
+$("#three").on("click", function() {
+    alert("Correct! Dont worry I wish I had one too");
+});
 
+$("#four").on("click", function () {
+    alert("Incorrect!");
+});
 
+$("#five").on("click", function () {
+    alert("Incorrect! The only thing suzuki has done right is the GSXR");
+});
+
+$("#six").on("click", function () {
+    alert("Correct!");
+});
+
+$("#seven").on("click", function () {
+    alert("Correct! Fast huh?")
+});
+
+$("#eight").on("click", function () {
+    alert("Incorrect. You dare doubt the H2R?");
+});
+
+//i dont plan on keeping these alerts, just easier for me to use when im done with the rest of the coding so i know which are correct and incorrect
